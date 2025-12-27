fca9c9fb837e4d23f3d65f799d6f922ca6888055
mohamedsaleh077 <mohammed.saleh.22707@gmail.com>
2025-12-28 01:07:04 +0200

    Forcing HTTPS connection

--- website/public/index.php
+++ website/public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+//for production only
+//if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+//    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+//    header('HTTP/1.1 301 Moved Permanently');
+//    header('Location: ' . $location);
+//    exit;
+//}
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/app/init.php';
 
 $app = new App;
