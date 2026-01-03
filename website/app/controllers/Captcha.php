7a9b547917f06f26e1be6613402273f79901d1e0
mohamedsaleh077 <mohammed.saleh.22707@gmail.com>
2026-01-03 15:56:48 +0200

    setting up Captcha

---
+++ website/app/controllers/Captcha.php
@@ -0,0 +1,20 @@
+<?php
+
+use Gregwar\Captcha\CaptchaBuilder;
+
+class Captcha extends Controller
+{
+
+    private $builder;
+    public function __construct()
+    {
+        $this->builder = new CaptchaBuilder;
+        $this->builder->build();
+        $_SESSION['captchaPhase'] = $this->builder->getPhrase();
+    }
+    public function img()
+    {
+        header('Content-type: image/jpeg');
+        $this->builder->output();
+    }
+}
