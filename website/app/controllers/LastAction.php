9742e3fbc9e77d51748e8d6d475bccb2ce14883e
mohamedsaleh077 <mohammed.saleh.22707@gmail.com>
2026-01-08 15:24:48 +0200

    refactor LastAction controller: remove unused methods and extract authentication-related logic to new AuthHelper trait

--- website/app/controllers/LastAction.php
+++ website/app/controllers/LastAction.php
@@ -4,9 +4,6 @@ namespace controllers;
 
 class LastAction
 {
-    public static function setLastAction(){
-        $_SESSION['last_action'] = time();
-    }
 
     public function getWaiting()
     {
@@ -17,10 +14,6 @@ class LastAction
         }
     }
 
-    public static function canProcess($interval){
-        if(!isset($_SESSION['last_action'])) return true;
-        if(time() - $_SESSION['last_action'] < $interval) return false;
-        return true;
-    }
+
 
 }
---
+++ website/app/core/traits/AuthHelper.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace core\traits;
+
+use core\Database;
+use core\QueryBuilder;
+
+use core\Controller;
+
+trait AuthHelper
+{
+    private $errors;
+    protected $user;
+    private $captcha;
+    private $csrf;
+    protected $username;
+    private $password;
+    private $SQL;
+
+    protected function inputCheck()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') die('Method not allowed');
+        $this->errors = new \core\Validation();
+
+        $this->username = $_POST['username'];
+        $this->password = $_POST['password'];
+        $this->csrf = $_POST['csrf'];
+        $this->captcha = $_POST['captcha'];
+
+        $this->errors->checkRequired(
+            [
+                "username" => $this->username,
+                "password" => $this->password,
+                "csrf" => $this->csrf,
+                "captcha" => $this->captcha
+            ]);
+        $this->errors->checkCSRF($this->csrf);
+        $this->errors->checkCaptchaPhase($this->captcha);
+        $this->errors->checkMaxLength(["username" => [50, $this->username], "password" => [50, $this->password]]);
+        $this->errors->checkMinLength(["username" => [3, $this->username], "password" => [8, $this->password]]);
+
+        // I wont check for password strongness, because it's not my job. since there is a JS unit for that!! so if
+        // user bypassed it to use a week password so, let him khs.
+
+    }
+
+    private function getUser()
+    {
+        if(empty($this->errors->getErrors())){
+            $this->SQL = new QueryBuilder();
+            $this->user = $this->SQL->select("users", ["id", "username", "password_hash"])->where([["username", "="]])->build()->execute([$this->username]);
+        }
+        return $this->user;
+    }
+
+    protected function addUser()
+    {
+        if (!empty($this->errors->getErrors())) return;
+        $options = [
+            'cost' => 12
+        ];
+        $hashedPwd = password_hash($this->password, PASSWORD_DEFAULT, $options);
+
+        $this->SQL->insert("users", ["username", "password_hash"])->Build()->execute([$this->username, $hashedPwd]);
+    }
+
+    protected function isUsernameTaken()
+    {
+        if ($this->getUser()[0]) $this->errors->addError("user_taken");
+    }
+
+    protected function isUsernameExist()
+    {
+        if (!$this->getUser()[0]) $this->errors->addError("user_not_found");
+    }
+
+    protected function checkPassword()
+    {
+        if (!password_verify($this->password, $this->user[0]['password_hash'])) {
+            $this->errors->addError("wrong_password");
+        }
+    }
+
+    protected function redirect($whenError = '', $whenDone = '')
+    {
+        if (!empty($this->errors->getErrors())) {
+            $_SESSION['user_id'] = null;
+            $_SESSION['username'] = null;
+            $_SESSION['errors'] = $this->errors->getErrors();
+            header('Location: /' . $whenError);
+        } else {
+            header('Location: /' . $whenDone);
+        }
+        die();
+    }
+
+    protected function setSessions()
+    {
+        $_SESSION['user_id'] = ($this->user[0]) ? $this->user[0]['id'] : Database::lastInsertId();
+        $_SESSION['username'] = $this->user[0]['username'] ?? $this->username;
+        $_SESSION['created_since'] = Controller::dateDiff($this->user[0]['created_at']);
+        $_SESSION['pfp'] = $this->user[0]['profile_picture'] ?? '/assets/default_pfp.svg';
+        $_SESSION['bio'] = $this->user[0]['bio'] ?? '';
+    }
+
+}
